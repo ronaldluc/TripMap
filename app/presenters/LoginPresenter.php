f28887cc574270b63dd52f8eb90c81a634f1fd35
norik16 <Rahaga16>
2016-01-30 02:10:24 +0100

    Added function out in Login

--- app/presenters/LoginPresenter.php
+++ app/presenters/LoginPresenter.php
@@ -54,4 +54,11 @@ class LoginPresenter extends Nette\Application\UI\Presenter
         }
     }
 
+    public function actionOut()
+    {
+        $this->getUser()->logout();
+        $this->flashMessage('Odhlášení bylo úspěšné.', 'success');
+        $this->redirect('Homepage:');
+    }
+
 }
