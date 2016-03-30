134544a5110201768da7e735f5724f7565ad93bb
norik16 <ron.norik@gmail.com>
2016-03-30 20:26:04 +0200

    Added editing and adding Categories

--- app/model/ManagementModel.php
+++ app/model/ManagementModel.php
@@ -26,7 +26,7 @@ class ManagementModel
         return $value;
     }
 
-    public function getFilteredTrips($user_id, $start, $end, $string)
+    public function getFilteredTrips($user_id, $start, $end, $string, $category_id)
     {
         $selection = $this->database->table('trip')->where('user_id = ?', $user_id);
 
@@ -40,13 +40,17 @@ class ManagementModel
 
         if ($end != NULL) {
             $selection = $selection->where('date <= ?', $end);
+        }
+
+        if ($category_id != NULL) {
+            $selection = $selection->where('category_id', $category_id);
         }
 
         $value = $selection->order('date DESC')->fetchAll();
         return $value;
     }
 
-    public function getFilteredId($user_id, $start, $end, $string)
+    public function getFilteredId($user_id, $start, $end, $string, $category_id)
     {
         $selection = $this->database->table('trip')->where('user_id = ?', $user_id);
 
@@ -60,6 +64,10 @@ class ManagementModel
 
         if ($end != NULL) {
             $selection = $selection->where('date <= ?', $end);
+        }
+
+        if ($category_id != NULL) {
+            $selection = $selection->where('category_id', $category_id);
         }
 
         $value = $selection->order('date DESC')->select('id')->fetchAll();
@@ -84,6 +92,7 @@ class ManagementModel
 
     public function editTrip($trip)
     {
+        if(!$trip->category) $trip->category = NULL;
         $selection = $this->database->table('trip');
         $selection->where('id = ?', $trip->id)->fetch()->update([
             'name' => $trip->name,
@@ -91,6 +100,7 @@ class ManagementModel
             'date' => $trip->date,
             'duration' => $trip->duration,
             'lenght' => $trip->lenght,
+            'category_id' => $trip->category,
         ]);
     }
 
@@ -103,5 +113,34 @@ class ManagementModel
             'green' => $values['green'],
             'blue' => $values['blue'],
         ]);
+    }
+
+    public function editCategory($values, $user_id)
+    {
+        $this->database->table('category')->where('id', $values['id'])->update([
+            'name' => $values['name'],
+            'red' => $values['red'],
+            'green' => $values['green'],
+            'blue' => $values['blue'],
+        ]);
+    }
+
+    public function loadCategories($user_id)
+    {
+        $categories = $this->database->table('category')->where('user_id', $user_id)->fetchAll();
+
+        return $categories;
+    }
+
+    public function getCategory($category_id)
+    {
+        $category = $this->database->table('category')->where('id', $category_id)->fetch();
+
+        return $category;
+    }
+
+    public function deleteCategory($category_id)
+    {
+        $this->database->table('category')->where('id', $category_id)->delete();
     }
 }
--- app/presenters/ManagementPresenter.php
+++ app/presenters/ManagementPresenter.php
@@ -46,12 +46,13 @@ class ManagementPresenter extends BasePresenter
                 'date' => $chosen->date->format('Y-m-d'),
                 'duration' => $chosen->duration,
                 'lenght' => $chosen->lenght,
+                'category' => $this->managementModel->getCategory($chosen->category_id),
             ]);
         }
 
         if ($this->filter != NULL)
         {
-            $this->template->trips = $this->managementModel->getFilteredTrips($this->user->id, $this->filter->from, $this->filter->to, $this->filter->name);
+            $this->template->trips = $this->managementModel->getFilteredTrips($this->user->id, $this->filter->from, $this->filter->to, $this->filter->name, $this->filter->category);
 //            $this->template->tripsId = $this->managementModel->getFilteredId($this->user->id, $this->filter->from, $this->filter->to, $this->filter->name);
 //            dump($this->template->tripsId);
 //            die;
@@ -70,6 +71,39 @@ class ManagementPresenter extends BasePresenter
         $this->redrawControl('newCategoryModal');
     }
 
+    public function renderCategory()
+    {
+        $id = $this->params['id'];
+        if ($id != NULL and $this->showNewCategoryModal == TRUE)
+        {
+            $chosen = $this->managementModel->getCategory($id);
+//            $color = '#'.base_convert($chosen->red, 10, 16).base_convert($chosen->green, 10, 16).base_convert($chosen->blue, 10, 16);
+            $color = Helpers::rgb2html($chosen->red, $chosen->green, $chosen->blue);
+            $this['newCategoryForm']->setDefaults([
+                'id' => $id,
+                'name' => $chosen->name,
+                'color' => $color,
+            ]);
+            $this->template->color = $color;
+        } else {
+            $this['newCategoryForm']->setDefaults([
+                'id' => NULL,
+                'name' => '',
+                'color' => '#000000',
+            ]);
+        }
+
+        $this->template->showModal = $this->showModal;
+
+        $this->template->categories = $this->managementModel->loadCategories($this->user->id);
+
+        $this->template->showNewCategoryModal = $this->showNewCategoryModal;
+
+        $this->redrawControl('categoryContainer');
+//        $this->redrawControl('editCategoryModal');
+        $this->redrawControl('newCategoryModal');
+    }
+
     public function handleDelete($id)
     {
         $this->managementModel->deleteTrip($id);
@@ -84,6 +118,13 @@ class ManagementPresenter extends BasePresenter
 
     public function createComponentEditTripForm($id)
     {
+        $categories = $this->managementModel->loadCategories($this->user->id);
+
+        $categoryNames[NULL] = 'Žádná';
+        foreach ($categories as $category) {
+            $categoryNames[$category->id] = $category->name;
+        }
+
         $form = new Nette\Application\UI\Form;
 
         $form->addText('name', 'Název')
@@ -106,7 +147,9 @@ class ManagementPresenter extends BasePresenter
         $form->addText('lenght', 'Délka trasy')
             ->setType('number');
 
-        $form->addSubmit('send', 'Vytvořit');
+        $form->addSelect('category', 'Kategorie', $categoryNames);
+
+        $form->addSubmit('send', 'Upravit');
 
         $form->onSuccess[] = array($this, 'editFormSucceeded');
 
@@ -126,10 +169,19 @@ class ManagementPresenter extends BasePresenter
 
     public function createComponentFilterForm()
     {
+        $categories = $this->managementModel->loadCategories($this->user->id);
+
+        $categoryNames[NULL] = 'Žádná';
+        foreach ($categories as $category) {
+            $categoryNames[$category->id] = $category->name;
+        }
+
         $form = new Nette\Application\UI\Form;
 
         $form->addText('name', 'Název')
             ->setType('search');
+
+        $form->addSelect('category', 'Kategorie', $categoryNames);
 
         $form->addText('from', 'Od')
             ->setType('date');
@@ -174,18 +226,15 @@ class ManagementPresenter extends BasePresenter
         die;
     }
 
-    public function handleNewCategory()
-    {
-        $this->showNewCategoryModal = TRUE;
-    }
-
     public function createComponentNewCategoryForm()
     {
         $form = new Nette\Application\UI\Form;
 
+        $form->addHidden('id');
 
         $form->addText('name', 'Název')
-            ->setRequired('Název je povinný');
+            ->setRequired('Název je povinný')
+            ->addRule(Form::MAX_LENGTH, 'Maximálně 42 znaků', 42);
 
         $form->addText('color', 'Barva')
             ->setType('color')
@@ -211,8 +260,28 @@ class ManagementPresenter extends BasePresenter
         $array['green'] = base_convert($color[3].$color[4], 16, 10);
         $array['blue'] = base_convert($color[5].$color[6], 16, 10);
 
-        $this->managementModel->newCategory($array, $this->user->id);
+        if ($values->id) {
+            $array['id'] = $values->id;
+            $this->managementModel->editCategory($array, $this->user->id);
+        } else {
+            $this->managementModel->newCategory($array, $this->user->id);
+        }
 
         $this->showNewCategoryModal = FALSE;
+    }
+
+    public function handleEditCategory($id)
+    {
+        $this->showNewCategoryModal = TRUE;
+    }
+
+    public function handleNewCategory()
+    {
+        $this->showNewCategoryModal = TRUE;
+    }
+
+    public function handleDeleteCategory($id)
+    {
+        $this->managementModel->deleteCategory($id);
     }
 }
